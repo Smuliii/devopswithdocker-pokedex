fab45daf578c3da42d6aa16389e03f5571a95cc9
Samuli Saarinen <samuli@crasman.fi>
2021-02-06 14:30:00 +0200

    Add new e2e test

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,10 @@ describe('Pokedex', () => {
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('open single pokemon page', () => {
+    cy.visit('http://localhost:5000')
+    cy.get('[href="/pokemon/pikachu"]').click()
+    cy.get('.pokemon-name').contains('pikachu')
+  })
 })
